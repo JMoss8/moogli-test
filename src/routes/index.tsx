eb0bc51cec605d5ff0b8c6eb67d2555bf0b9a14a
Ján Mach <mach.jano@gmail.com>
2024-09-08 00:17:32 +0200

    Home - notes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,26 @@
 const Home = () => {
-  return <h1>Hi wrld</h1>
+  return (
+    <>
+      <h3>Wins</h3>
+      <ul>
+        <li>
+          In the <code>datasets</code> folder you can find your data as <code>.csv</code>. I wrote a parser to get it as
+          JSON. Feel free to change the values "in the <code>datasets</code>" and run <code>yarn dev</code>, you'll see
+          your new values in the app.
+        </li>
+      </ul>
+      <h3>Problems</h3>
+      <ul>
+        <li></li>
+        <li>The Sunburst chart doesn't support Legend component for filtering, so I had to create a custom one</li>
+      </ul>
+      <h3>TODOs</h3>
+      <ul>
+        <li>Responsiveness (right now the app supports only PC with "normal" size screens)</li>
+        <li>Automated tests</li>
+      </ul>
+    </>
+  )
 }
 
 export default Home
